d4a0b1688b81b841b4eb44ab9038a270944fb29a
Fidel Salazar <fidelsf91@gmail.com>
2022-08-20 19:33:15 -0500

    Added classes for states, added state machine for counter button, added title service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Status } from '../status';
+import {Title} from "@angular/platform-browser";
 
 @Component({
     selector: 'app-home',
@@ -7,28 +9,70 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
     countdown = new Audio();
+    counterClass = "ready";
     counterBtnText = "Start";
 
-    constructor() { }
+    state = Status.Ready;
+
+    sets = 5;
+    seconds = 10;
+    rest = 5;
+
+    constructor(private titleService:Title) { }
 
     ngOnInit(): void {
+        this.titleService.setTitle("Kegel App");
         this.countdown.src = "../../../assets/audio/countdown.mp3";
         this.countdown.load();
     }
 
     counterBtnClick(): void {
-        this.countdown.play();
-        let num = 4;
-        this.counterBtnText = num.toString();
+        switch(this.state) {
 
-        let intervalId = setInterval(() => {
-            num--;
-            this.counterBtnText = num.toString();
-            if(num === 0) clearInterval(intervalId)
-        }, 1000)
+            case Status.Ready: {
+                this.state = Status.Starting;
+                this.counterClass = "starting";
+                this.countdown.play();
+                let num = 3;
+                
+                this.counterBtnText = num.toString();
+                this.titleService.setTitle(this.counterBtnText);
 
+                let intervalId = setInterval(() => 
+                {
+                    num--;
+                    this.counterBtnText = num.toString();
+                    this.titleService.setTitle(this.counterBtnText);
+            
+                    if(num === 0) {
+                        this.state = Status.Counting;
+                        clearInterval(intervalId);
+                        this.countSet();
+                    }
+                }, 1000)
+                break;
+            }
+
+            case Status.Starting: {
+                this.state = Status.Ready;
+                break;
+            }
+
+            case Status.Counting: {
+                this.state = Status.Ready;
+                break;
+            }
+
+            case Status.Resting: {
+                this.state = Status.Ready;
+                break;
+            }
+        }
+    }
+
+    countSet(): void {
         
-
+        this.titleService.setTitle(this.counterBtnText);
         
     }
 }
