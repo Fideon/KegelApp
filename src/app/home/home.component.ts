0c5f43730890541b650305aebd1b0f145b5ee5c2
Fidel Salazar <fidelsf91@gmail.com>
2022-08-22 08:54:22 -0500

    Added ticking audio and logic for counting sets

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,22 +8,25 @@ import {Title} from "@angular/platform-browser";
     styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-    countdown = new Audio();
+    starting = new Audio();
+    tick = new Audio();
     counterClass = "ready";
     counterBtnText = "Start";
 
     state = Status.Ready;
 
     sets = 5;
+    currentSet = 1;
     seconds = 10;
     rest = 5;
 
     constructor(private titleService:Title) { }
 
     ngOnInit(): void {
-        this.titleService.setTitle("Kegel App");
-        this.countdown.src = "../../../assets/audio/countdown.mp3";
-        this.countdown.load();
+        this.starting.src = "../../../assets/audio/starting.mp3";
+        this.tick.src = "../../../assets/audio/tick.mp3";
+        this.starting.load();
+        this.tick.load();
     }
 
     counterBtnClick(): void {
@@ -32,10 +35,10 @@ export class HomeComponent implements OnInit {
             case Status.Ready: {
                 this.state = Status.Starting;
                 this.counterClass = "starting";
-                this.countdown.play();
+                this.starting.play();
                 let num = 3;
                 
-                this.counterBtnText = num.toString();
+                this.counterBtnText = num.toString();   
                 this.titleService.setTitle(this.counterBtnText);
 
                 let intervalId = setInterval(() => 
@@ -44,10 +47,10 @@ export class HomeComponent implements OnInit {
                     this.counterBtnText = num.toString();
                     this.titleService.setTitle(this.counterBtnText);
             
-                    if(num === 0) {
+                    if (num === 0) {
                         this.state = Status.Counting;
+                        this.countAction();
                         clearInterval(intervalId);
-                        this.countSet();
                     }
                 }, 1000)
                 break;
@@ -70,9 +73,63 @@ export class HomeComponent implements OnInit {
         }
     }
 
-    countSet(): void {
-        
-        this.titleService.setTitle(this.counterBtnText);
-        
+    countAction(): void {
+        switch(this.state) {
+            case Status.Starting: {
+                this.state = Status.Ready;
+                break;
+            }
+
+            case Status.Counting: {
+                this.counterBtnText = this.seconds.toString();
+                this.titleService.setTitle("Kegeling... " + this.counterBtnText + ". Set " + this.currentSet + " of " + this.sets);
+                this.counterClass = "counting"
+
+                let countdown = this.seconds;
+                this.tick.play();
+
+                let intervalId = setInterval(() => 
+                {
+                    countdown--;
+                    this.tick.play();
+                    this.counterBtnText = countdown.toString();
+                    this.titleService.setTitle("Kegeling... " + this.counterBtnText + ". Set " + this.currentSet + " of " + this.sets);
+                
+                    if (countdown === 0) {
+                        this.state = Status.Resting;
+                        clearInterval(intervalId);
+                        this.countAction();
+                    }
+                }, 1000)
+                break;
+            }
+
+            case Status.Resting: {
+                this.counterBtnText = this.rest.toString();
+                this.titleService.setTitle("Resting... " + this.counterBtnText + ". Set " + this.currentSet + " of " + this.sets);
+                this.counterClass = "resting"
+
+                let countdown = this.rest;
+
+                let intervalId = setInterval(() => 
+                {
+                    countdown--;
+                    this.counterBtnText = countdown.toString();
+                    this.titleService.setTitle("Resting... " + this.counterBtnText + ". Set " + this.currentSet + " of " + this.sets);
+                
+                    if (countdown === 0) {
+                        this.currentSet++;
+                        if (this.currentSet > this.sets) {
+                            this.state = Status.Ready;
+                        } else {
+                            this.state = Status.Counting;
+                        }
+                        clearInterval(intervalId);
+                        this.countAction();
+                    }
+                }, 1000)
+                break;
+            }
+        }
     }
 }
